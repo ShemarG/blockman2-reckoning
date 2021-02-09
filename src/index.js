6d5f1e8258de49a8aba7d514dbc10dd178949841
Jarr1t <aliceajarrit@gmail.com>
2021-02-09 00:41:58 -0500

    moved functions and made CSS

--- src/index.js
+++ src/index.js
@@ -1,5 +1,50 @@
 const game = new Game(document.getElementById('el'));
 
+function generateHUD() {
+  game.HUD = {};
+  const healthBar = document.createElement('div');
+  healthBar.style.position = 'absolute';
+  healthBar.style.display = 'flex';
+  healthBar.style.height = '2em';
+  healthBar.style.color = 'white';
+  game.HUD.healthBar = healthBar;
+  const label = document.createElement('span');
+  label.textContent = 'Health';
+  label.style.fontFamily = 'Goldman';
+  healthBar.append(label);
+  const bar = document.createElement('div');
+  bar.style.display = 'flex';
+  game.HUD.health = bar;
+  for (let i = 0; i < game.player.stats.health; i++) addHealthUnit();
+  healthBar.append(bar);
+  game.area.append(healthBar);
+  const pause = document.createElement('span');
+  pause.classList.add('pause-button')
+  pause.innerText = 'Pause';
+  game.area.append(pause);
+  pause.addEventListener('click', () => {
+    if (pause.innerText === 'Pause' && game.player.stats.health !== 0) {
+      debugger;
+      pause.innerText = 'Resume';
+      game.togglePause();
+    } else if (pause.innerText === 'Resume' && game.player.stats.health !== 0) {
+      pause.innerText = 'Pause';
+      game.togglePause();
+    }
+  });
+}
+generateHUD()
+
+function addHealthUnit() {
+  const healthUnit = document.createElement('div');
+  healthUnit.style.width = '1em';
+  healthUnit.style.height = '1em';
+  healthUnit.style.backgroundColor = 'red';
+  healthUnit.style.margin = '0 0.1em 0 0.1em';
+  game.HUD.health.append(healthUnit);
+}
+
+
 const homeScreen = document.getElementById('homescreen');
 
 const startButton = document.getElementById('start');
@@ -38,25 +83,25 @@ restartButton.innerText = 'Try Again';
 restartButton.style.display = 'none';
 restartButton.style.color = 'white';
 
-const levelupScreen = document.getElementById('levelupScreen')
-levelupScreen.style.margin = '1.5em'
-const level = document.createElement('span')
-levelupScreen.append(level)
-level.innerText = 'Level Complete'
-level.style.fontFamily = 'Goldman'
-level.style.marginLeft = '2.5em'
-level.style.marginTop = '4em'
-level.style.fontSize = '3em'
-const skillPoints = document.createElement('div')
-levelupScreen.append(skillPoints)
-skillPoints.innerText = 'Skill Points : '
-skillPoints.style.fontFamily = 'Goldman'
-skillPoints.style.fontSize = '2em'
-skillPoints.style.marginTop = '1.5em'
-skillPoints.style.marginLeft = '7em'
-const healthPoints = document.createElement('span')
-healthPoints.innerText = 'Health Points'
-levelupScreen.append(healthPoints)
+// const levelupScreen = document.getElementById('levelupScreen')
+// levelupScreen.style.margin = '1.5em'
+// const level = document.createElement('span')
+// levelupScreen.append(level)
+// level.innerText = 'Level Complete'
+// level.style.fontFamily = 'Goldman'
+// level.style.marginLeft = '2.5em'
+// level.style.marginTop = '4em'
+// level.style.fontSize = '3em'
+// const skillPoints = document.createElement('div')
+// levelupScreen.append(skillPoints)
+// skillPoints.innerText = 'Skill Points : '
+// skillPoints.style.fontFamily = 'Goldman'
+// skillPoints.style.fontSize = '2em'
+// skillPoints.style.marginTop = '1.5em'
+// skillPoints.style.marginLeft = '7em'
+// const healthPoints = document.createElement('span')
+// healthPoints.innerText = 'Health Points'
+// levelupScreen.append(healthPoints)
 
 startButton.addEventListener('click', () => {
   game.togglePause();
